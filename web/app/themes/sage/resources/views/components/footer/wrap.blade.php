a58fbde9fb6fa99b0fd59009a93bfe69d810caf8
Mac <mac@MacBook-Pro-Mac.local>
2025-11-05 17:33:45 +0300

    footer-links

--- web/app/themes/sage/resources/views/components/footer/wrap.blade.php
+++ web/app/themes/sage/resources/views/components/footer/wrap.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="container">
         <div class="footer-mobile">
-            <span class="footer-mobile__link">Политика конфиденциальности</span>
-            <span class="footer-mobile__link">Пользовательское соглашение</span>
+            <a href="/politika-konfidencialnosti" class="footer-mobile__link">Политика конфиденциальности</a>
+            <a href="/polzovatelskoe-soglashenie" class="footer-mobile__link">Пользовательское соглашение</a>
         </div>
         <div class="footer__container">
             <div class="footer__left">
@@ -10,8 +10,8 @@
             </div>
 
             <div class="footer__right">
-                <span class="footer__link">Политика конфиденциальности</span>
-                <span class="footer__link">Пользовательское соглашение</span>
+                <a href="/politika-konfidencialnosti" class="footer__link">Политика конфиденциальности</a>
+                <a href="/polzovatelskoe-soglashenie" class="footer__link">Пользовательское соглашение</a>
                 <span class="footer__text">2025</span>
             </div>
         </div>
